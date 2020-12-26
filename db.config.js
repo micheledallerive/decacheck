379a291bf80839c8fa3a2b2301103fa0c1b31ab4
micheledallerive <dallerivemiki@gmail.com>
2020-12-26 12:35:05 +0100

    1.0

---
+++ db.config.js
@@ -0,0 +1,7 @@
+// URL: mysql://b372780c5a8713:61f8795b@eu-cdbr-west-03.cleardb.net/heroku_e2ad04f08a7e180
+module.exports={
+    HOST: "eu-cdbr-west-03.cleardb.net",
+    USER: "b372780c5a8713",
+    PASSWORD: "61f8795b",
+    DB: "heroku_e2ad04f08a7e180"
+}
